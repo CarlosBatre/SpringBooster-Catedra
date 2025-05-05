494d5b9f5643aea5025823cc69e26eb487ede144
RB230288 <carlosbatres.gg@gmail.com>
2025-05-05 13:57:11 -0600

    Configuracion de la Entidad Producto

--- spring-api/src/main/java/sv/edu/udb/repository/ProductoRepository.java
+++ spring-api/src/main/java/sv/edu/udb/repository/ProductoRepository.java
@@ -1,4 +1,9 @@
 package sv.edu.udb.repository;
 
-public interface ProductoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sv.edu.udb.repository.domain.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
