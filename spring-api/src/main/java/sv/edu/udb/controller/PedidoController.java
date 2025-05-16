9117c95daf9f89a004a9fde32f67a1e033ab89dc
RB230288 <carlosbatres.gg@gmail.com>
2025-05-15 19:12:26 -0600

    update

--- spring-api/src/main/java/sv/edu/udb/controller/PedidoController.java
+++ spring-api/src/main/java/sv/edu/udb/controller/PedidoController.java
@@ -2,3 +2,4 @@ package sv.edu.udb.controller;
 
 public class PedidoController {
 }
+
--- spring-api/src/main/java/sv/edu/udb/controller/response/UserResponse.java
+++ spring-api/src/main/java/sv/edu/udb/controller/response/UserResponse.java
@@ -8,7 +8,7 @@ import lombok.experimental.FieldNameConstants;
 @Getter
 @Setter
 @Builder(toBuilder = true) //Es para poder modificar el objeto luego de creado
-@FieldNameConstants //Para generar constantes con los nombres de las propiedades
+@FieldNameConstants 
 public class UserResponse {
 
     private String username;
