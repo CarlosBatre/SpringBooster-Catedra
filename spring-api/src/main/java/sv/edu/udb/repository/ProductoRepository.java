d69783d1dc033463b3b8a2a952aa9fdd684e6f22
RB230288 <125234995+CarlosBatre@users.noreply.github.com>
2025-05-15 19:10:42 -0600

    Revert "Back end"

--- spring-api/src/main/java/sv/edu/udb/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package sv.edu.udb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sv.edu.udb.repository.domain.Producto;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
