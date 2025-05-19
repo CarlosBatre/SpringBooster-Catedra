bb272ff373b02d815fd73c3394de49af781d94c0
RB230288 <carlosbatres.gg@gmail.com>
2025-05-19 13:12:18 -0600

    Pedido Request y Response

---
+++ src/main/java/com/garajeideas/login/jpaLogin/controller/response/PedidoResponse.java
@@ -0,0 +1,33 @@
+package com.garajeideas.login.jpaLogin.controller.response;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PedidoResponse {
+    private Long id;
+    private String nombreCliente;
+    private String direccion;
+    private String telefono;
+    private String metodoPago;
+    private String estado;
+    private LocalDate fechaPedido;
+    private Double total;
+    private List<DetallePedidoDTO> detalles;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DetallePedidoDTO {
+        private Long productoId;
+        private String nombreProducto;
+        private int cantidad;
+    }
+}
