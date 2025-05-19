67db410f9b5b096bec9abab818cd42baf33052f5
RB230288 <carlosbatres.gg@gmail.com>
2025-05-19 13:08:28 -0600

    Create Pedido y Detalle Pedido

---
+++ src/main/java/com/garajeideas/login/jpaLogin/entity/Pedido.java
@@ -0,0 +1,36 @@
+package com.garajeideas.login.jpaLogin.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombreCliente;
+
+    private String direccion;
+
+    private String telefono;
+
+    private String metodoPago;
+
+    private String estado = "Pedido Realizado";
+
+    private LocalDate fechaPedido;
+
+    private Double total;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<DetallePedido> detalles;
+}
