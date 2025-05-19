50c644c71d6734627521ebdef93da541abb0ccd8
RB230288 <carlosbatres.gg@gmail.com>
2025-05-19 13:18:32 -0600

    Create PedidoController.java

---
+++ src/main/java/com/garajeideas/login/jpaLogin/controller/PedidoController.java
@@ -0,0 +1,36 @@
+package com.garajeideas.login.jpaLogin.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import com.garajeideas.login.jpaLogin.controller.request.PedidoRequest;
+import com.garajeideas.login.jpaLogin.controller.response.PedidoResponse;
+import com.garajeideas.login.jpaLogin.service.PedidoService;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "pedidos")
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public PedidoResponse realizarPedido(@Valid @RequestBody PedidoRequest request) {
+        return pedidoService.realizarPedido(request);
+    }
+    @GetMapping
+    public List<PedidoResponse> obtenerPedidos() {
+        return pedidoService.listarPedidos();
+    }
+    @GetMapping("/{id}")
+    public PedidoResponse obtenerPedidoPorId(@PathVariable Long id) {
+        return pedidoService.obtenerPedidoPorId(id);
+    }
+
+}
