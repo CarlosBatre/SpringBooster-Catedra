cd5067255179ee15db65d4bd8cedc92f895dadd6
RB230288 <carlosbatres.gg@gmail.com>
2025-05-19 12:30:30 -0600

    Producto Repository

--- src/main/java/com/garajeideas/login/jpaLogin/repository/ProductoRepository.java
+++ src/main/java/com/garajeideas/login/jpaLogin/repository/ProductoRepository.java
@@ -2,7 +2,7 @@ package com.garajeideas.login.jpaLogin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sv.edu.udb.repository.domain.Producto;
+import com.garajeideas.login.jpaLogin.entity.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
