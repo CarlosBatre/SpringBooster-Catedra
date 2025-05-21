4cdb082925f645d3a5dfbbd9fdeab3cc7566fcf5
RB230288 <carlosbatres.gg@gmail.com>
2025-05-21 09:57:56 -0600

    Update ProductoController.java

--- src/main/java/com/garajeideas/login/jpaLogin/controller/ProductoController.java
+++ src/main/java/com/garajeideas/login/jpaLogin/controller/ProductoController.java
@@ -2,12 +2,14 @@ package com.garajeideas.login.jpaLogin.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.*;
 import com.garajeideas.login.jpaLogin.controller.request.ProductoRequest;
 import com.garajeideas.login.jpaLogin.controller.response.ProductoResponse;
 import com.garajeideas.login.jpaLogin.service.ProductoService;
 
-import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
@@ -20,16 +22,15 @@ public class ProductoController {
 
     private final ProductoService productoService;
 
+    @GetMapping
+    public PagedModel<ProductoResponse>findAll(Pageable pageable) {return productoService.findAll(pageable);}
+
     @GetMapping(path = "{id}")
-    public ProductoResponse findProductoById(@PathVariable(name = "id") final Long id) {
-        return productoService.findById(id);
-    }
+    public ProductoResponse findProductoById(@PathVariable(name = "id") final Long id) {return productoService.findById(id);}
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public ProductoResponse saveProducto(@Valid @RequestBody final ProductoRequest request) {
-        return productoService.save(request);
-    }
+    public ProductoResponse saveProducto(@Valid @RequestBody final ProductoRequest request) {return productoService.save(request);}
 
     @PutMapping(path = "{id}")
     public ProductoResponse updateProducto(@PathVariable(name = "id") final Long id,
