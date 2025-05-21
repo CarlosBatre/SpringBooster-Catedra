60515ec81a52c30d505f9ca7fb8e569434a45936
RB230288 <carlosbatres.gg@gmail.com>
2025-05-21 09:54:17 -0600

    Update ProductoService.java

--- src/main/java/com/garajeideas/login/jpaLogin/service/ProductoService.java
+++ src/main/java/com/garajeideas/login/jpaLogin/service/ProductoService.java
@@ -1,11 +1,12 @@
 package com.garajeideas.login.jpaLogin.service;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 import com.garajeideas.login.jpaLogin.controller.response.ProductoResponse;
 import com.garajeideas.login.jpaLogin.controller.request.ProductoRequest;
 import java.util.List;
 
 public interface ProductoService {
-    List<ProductoResponse> findAll();
+    PagedModel<ProductoResponse> findAll(Pageable pageable);
     ProductoResponse findById(final Long id);
     ProductoResponse save(final ProductoRequest productoRequest);
     ProductoResponse update(final Long id, final ProductoRequest productoRequest);
