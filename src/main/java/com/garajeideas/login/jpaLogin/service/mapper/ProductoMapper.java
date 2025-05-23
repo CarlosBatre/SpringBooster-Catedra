40b07b7d1bd9938cfb50a14b1c3cedb622602336
RB230288 <carlosbatres.gg@gmail.com>
2025-05-22 22:57:50 -0600

    Update ProductoMapper.java

--- src/main/java/com/garajeideas/login/jpaLogin/service/mapper/ProductoMapper.java
+++ src/main/java/com/garajeideas/login/jpaLogin/service/mapper/ProductoMapper.java
@@ -4,10 +4,12 @@ import org.mapstruct.Mapper;
 import com.garajeideas.login.jpaLogin.controller.response.ProductoResponse;
 import com.garajeideas.login.jpaLogin.controller.request.ProductoRequest;
 import com.garajeideas.login.jpaLogin.entity.Producto;
-import java.util.List;
+import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface ProductoMapper {
+
     ProductoResponse toProductoResponse(final Producto data);
     Producto toProducto(final ProductoRequest productoRequest);
 }
