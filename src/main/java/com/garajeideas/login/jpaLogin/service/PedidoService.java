fa53f8ec6432f128ba6e9c3b54f2ff3add5f41cb
RB230288 <carlosbatres.gg@gmail.com>
2025-05-23 01:29:22 -0600

    Update PedidoService.java

--- src/main/java/com/garajeideas/login/jpaLogin/service/PedidoService.java
+++ src/main/java/com/garajeideas/login/jpaLogin/service/PedidoService.java
@@ -8,5 +8,6 @@ public interface PedidoService {
     PedidoResponse realizarPedido(PedidoRequest request);
     PagedModel<PedidoResponse> listarPedidos(Pageable pageable);
     PedidoResponse obtenerPedidoPorId(Long id);
+    PedidoResponse actualizarPedido(Long id, PedidoRequest request);
 
 }
