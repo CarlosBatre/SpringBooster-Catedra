c8196d5f21c1f432e0752fecd22eb1bc8253b6d5
RB230288 <carlosbatres.gg@gmail.com>
2025-05-23 11:11:53 -0600

    Update LoginRequest.java

--- src/main/java/com/garajeideas/login/jpaLogin/controller/LoginRequest.java
+++ src/main/java/com/garajeideas/login/jpaLogin/controller/LoginRequest.java
@@ -1,9 +1,12 @@
 package com.garajeideas.login.jpaLogin.controller;
 
-import lombok.Data;
-
-@Data
 public class LoginRequest {
     private String email;
     private String password;
+
+    // Getters and Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
