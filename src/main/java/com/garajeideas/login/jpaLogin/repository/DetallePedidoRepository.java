e6c2f95725bb1f628aaf1814cd8f7335bc416dfa
RB230288 <carlosbatres.gg@gmail.com>
2025-05-24 15:28:42 -0600

    Delete DetallePedidoRepository.java

--- src/main/java/com/garajeideas/login/jpaLogin/repository/DetallePedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.garajeideas.login.jpaLogin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.garajeideas.login.jpaLogin.entity.DetallePedido;
-
-public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Long> {
-}
