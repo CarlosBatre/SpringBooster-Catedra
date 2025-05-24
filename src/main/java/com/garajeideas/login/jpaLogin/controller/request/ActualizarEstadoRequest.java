25ae49d1646e78e14560575a8091eee3547d83c0
RB230288 <carlosbatres.gg@gmail.com>
2025-05-24 15:28:39 -0600

    Create ActualizarEstadoRequest.java

---
+++ src/main/java/com/garajeideas/login/jpaLogin/controller/request/ActualizarEstadoRequest.java
@@ -0,0 +1,10 @@
+package com.garajeideas.login.jpaLogin.controller.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ActualizarEstadoRequest {
+    private String estado;
+}
